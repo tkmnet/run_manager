d3ac84aa6071473a163fa43bc1eb670f84c5fe71
Shunki Takami <tkmnet@users.noreply.github.com>
2017-10-23 07:06:51 +0900

    Update

---
+++ MainPage.php
@@ -0,0 +1,24 @@
+<?php
+namespace rrsoacis\apps\tkmnet\run_manager;
+
+use rrsoacis\component\common\AbstractPage;
+use rrsoacis\system\Config;
+
+class SettingsPage extends AbstractPage
+{
+	public function controller($params)
+	{
+		$this->setTitle("Run Manager");
+		$this->printPage();
+	}
+
+	function body()
+	{
+		self::writeContentHeader("Run Manager");
+
+		self::beginContent();
+?>
+<?php
+		self::endContent();
+	}
+}
