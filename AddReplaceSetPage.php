f2bbc27be3a3d0c7bbce82025a7c406b7e766a4d
Shunki Takami <tkmnet@users.noreply.github.com>
2018-07-02 01:08:53 +0000

    update

--- AddReplaceSetPage.php
+++ AddReplaceSetPage.php
@@ -154,7 +154,7 @@ class AddReplaceSetPage extends AbstractPage
 						<div class="col-sm-3">
 							<div class="input-group">
 								<input type="text" placeholder="Auto fill by Group" class="form-control" name="autofill_group" value="">
-								<span class="input-group-btn"><button type="button" class="btn btn-info btn-flat">Load</button></span>
+								<span class="input-group-btn"><button type="submit" class="btn btn-info btn-flat">Load</button></span>
 							</div>
 						</div>
 					</div>
